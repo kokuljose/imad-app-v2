01517a96c44f0e2dd30553d8c523f6efa8a88f83
Kokul Jose <kokuljose@outlook.com>
2017-02-14 01:17:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,11 +2,11 @@ console.log('Loaded!');
 var element=document.getElementById('main');
 element.innerHTML="kokul jose";
 var img=document.getElementById(madi);
-var mleft=0;
+var marginLeft=0;
 function moveright()
 {
-    mleft-mleft+10;
-    img.style.marginLeft=mleft+"px";
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+"px";
 }
 img.onclick = function()
 {
