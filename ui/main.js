1f004f59f778eb49042027a29e68ef9d0b1900ce
Kokul Jose <kokuljose@outlook.com>
2017-02-15 02:05:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,6 @@
-console.log('Loaded!');
-var element=document.getElementById('main');
-element.innerHTML="kokul jose";
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveright()
-{
-    marginLeft=marginLeft+1;
-    img.style.marginLeft=marginLeft+"px";
-}
-img.onclick = function()
-{
-    var interval=setInterval(moveright,50);
-}
+var button=getElementById("count");
+button.onclick=function() {
+    count=counr+1;
+    var span =document.getElementById=("c");
+    span.innerHTML=count.toString();
+};
