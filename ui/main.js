ef010a4cc1591c9676b747a677a27387238319a5
Kokul Jose <kokuljose@outlook.com>
2017-03-09 23:25:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,1 @@
-var button=document.getElementById("count");
-var coun=0;
-button.onclick=function(){
-     coun=coun+1;
-     var span=document.getElementById("c");
-     span.innerHTML=counter.toString();
-};
+console.log('Loaded!');
