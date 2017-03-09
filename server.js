8e0ff7d23aa7103fd61dc6fb242152062c8d871e
Kokul Jose <kokuljose@outlook.com>
2017-03-09 23:23:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,56 +5,15 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-var article={
-            a1:{
-            title:'kokul 1',
-            date:"sept1"
-            },
-            a2:{
-            title:'kokul 2',
-            date:"sept2"
-            },
-            a3:{
-            title:'kokul 3',
-            date:"sept3"
-            }
-    };
 
-function ctemplate(data)
-{
-    var title=data.title;
-    var date=data.date;
-    var template=` <!DOCTYPE html>
-    <html>
-    <head>
-    <title>kokul</title>
-    </head>
-    <body>
-    <h1>hello</h1>
-        ${title}
-        ${date}
-    </body>
-    </html>`;
-    return template;
-}
-var count=0;
-app.get("/counter", function(req,res) {
-    count = count+1;
-    res.send(count.toString());
-});
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/:arname',function (req, res){
-    var arname=req.params.arname;
-    res.send(ctemplate(article[arname]));
-});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
