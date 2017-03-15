8de4176ee5b68ae2e20d694fe7ced1b14b21fc33
Kokul Jose <kokuljose@outlook.com>
2017-03-15 23:56:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article1',function(req,res){
-    res.sendFile(path.join(__dirname,'ui','article1.txt'));
+    res.sendFile(path.join(__dirname,'ui','article1.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
