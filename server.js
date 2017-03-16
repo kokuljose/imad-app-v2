80f808793da3a672faf0f259cb81f73b98a20417
Kokul Jose <kokuljose@outlook.com>
2017-03-16 22:59:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -43,7 +43,9 @@ app.get('/:articleName', function (req, res) {
     var articleName=req.params.articleName;
   res.send(createTemplate(articles[articleName]));
 });
-
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
