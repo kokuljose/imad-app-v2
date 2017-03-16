13e04dd546292818fbf0e875b53fe3d60a2a9051
Kokul Jose <kokuljose@outlook.com>
2017-03-16 23:33:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ element.innerHTML="newvalue";
 var img=document.getElementById("madi");
 function moveRight(){
     marginLeft=marginLeft+10;
-    img.style.marginLeft=marginleft+"px";
+    img.style.marginLeft=marginLeft+"px";
 }
 img.onclick=function() {
     var interval=setInterval(moveRight,100);
