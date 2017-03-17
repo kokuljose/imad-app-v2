702bdda0ae2a5a5e9c2fcb0bb23e865292906686
Kokul Jose <kokuljose@outlook.com>
2017-03-17 22:16:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,7 +19,12 @@ var nameInput=document.getElementById("name");
 var name=nameInput.value;
 var submit = document.getElementById("submit_btn");
 submit.onclick=function() {
-    var names=["names1","name2","name3","name4"];
+        var request=new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(request.readyState===XMLHttpRequest.DONE){
+            if(request.status===200){
+                var names=request.responseText;
+                names=JSON.parse(names);
     var list="";
     for (var i=0;i<names.length;i++){
         list+="<li>"+names[i]
@@ -27,4 +32,11 @@ submit.onclick=function() {
 }
 var ul=document.getElementById("namelist");
 ul.innerHTML=list;
+            }
+        }
+    };
+   
+    request.open("GET","http://kokuljose.imad.hasura-app.io/submit-name?name="+name,true);
+    request.send(null);
+    
 };
