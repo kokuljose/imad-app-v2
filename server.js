7c03db88f572fda382758a45fddd05551537f087
Kokul Jose <kokuljose@outlook.com>
2017-03-20 01:59:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ function hash(input,salt){
 }
 
 app.get("/hash/:input", function(req,res){
-   var hashedString=hash(req.params.input,"this is random string"); 
+   var hashedString=hash(req.params.input,"thisisrandomstring"); 
    res.send(hashedString);
 });
 
