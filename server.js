5d9aeac83d85636c437bda7d8eb255cb3c2cf22f
Kokul Jose <kokuljose@outlook.com>
2017-03-20 00:46:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var app = express();
 app.use(morgan('combined'));
 function hash(input,salt){
     var hashed=crypto.pbkdf25ync(input,salt,10000,512,"sha512");
-    return hashed;
+    return hashed.toString('hex');
 }
 
 app.get("/hash/:input", function(req,res){
