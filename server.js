6e42700afe3412e35d96c6cfd5da9cade8518c61
Kokul Jose <kokuljose@outlook.com>
2017-03-19 23:05:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var config= {
     database:"kokuljose",
     host:"db.imad.hasura-app.io",
     port:"5432",
-    password: process.env.DB_PASSWORD
+    password: 'db-kokuljose-52904'
 };
 var app = express();
 app.use(morgan('combined'));
