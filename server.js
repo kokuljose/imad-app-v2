acec5cc8dc9a5d8b80a3670ccc669885dbe1f37e
Kokul Jose <kokuljose@outlook.com>
2017-03-19 22:46:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var Pool=require("pg").Pool;
 var config= {
     user:"kokuljose",
     database:"kokuljose",
-    host:"db.imad.hasura_app.io",
+    host:"db.imad.hasura-app.io",
     port:"5432",
     password: process.env.DB_PASSWORD
 };
